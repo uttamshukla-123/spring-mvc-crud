68169b6a81aa70986c27be9a511e76ba3859e018
Uttam2021 <uttamshukla12345@gmail.com>
2023-10-12 11:52:31 +0530

    post put and delete opertaion perfomred

--- src/main/java/com/luv2code/springboot/thymeleafdemo/ThymeleafdemoApplication.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/ThymeleafdemoApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ThymeleafdemoApplication {
+public class
+ThymeleafdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ThymeleafdemoApplication.class, args);
